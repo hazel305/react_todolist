75ad8fb2cb68651ba66d85c1c9afe0f00d271f38
Hyunny <hazel413@naver.com>
2023-10-27 11:24:00 +0900

    [FEAT]리스트 체크박스 체크

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Todo from './components/Todo';
 
@@ -11,19 +12,41 @@ function App() {
     {id:2, text:"learn chapter2", checked:false},
     {id:3, text:"learn chapter3", checked:false},
   ]);
+  const [todoId, setTodoId] = useState(2);
 
   let todos = todo.map(item=>(
     <Todo key={item.id} item={item}/>
   ))
+
+  let addTodo = (item)=>{
+    let newTodos = [...todo];
+    //todoId는 const라서 복사하고 가져오기
+    let newId  = todoId +1;
+    setTodoId(newId+1);
+   newTodos.push({id:newId, text:item, checked:false});
+   setTodo(newTodos);
+  //  setTodo("");
+  document.getElementById('todo').value='';
+  }
+
+  const handleSubmit = (e)=>{
+  e.preventDefault();
+  // console.log(e.target.todo.value);
+   addTodo(e.target.todo.value);
+  }
   return (
     <div className="container">
     <h2>todo List</h2>
-    <Form>
-      <Form.Group className="mb-3" controlId="todo1">
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="todo">
         <Form.Label>Add todo</Form.Label>
-        <Form.Control type="text" placeholder="add todo" />
+        <Form.Control type="text" placeholder="add todo" name="todo"/>
       </Form.Group>
+      <Button variant="primary" type="submit">
+        Add
+      </Button>
     </Form>
+
     <div>
       {todos}
     </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
+// import Form from 'react-bootstrap/Form';
 
 const Todo = ({item}) => {
+    const [isChecked, setIsChecked] = useState(false);
+
+    let className = 'form-check-label';
+    let deco = {};
+    if(isChecked){
+        className += ' text-muted';
+        deco = {textDecoration:'line-through'}
+    }
+
+    const handleCheckboxClick = ()=>{
+        setIsChecked(!isChecked);
+        // setIsChecked((prevChecked) => !prevChecked);
+      
+        // console.log(isChecked);
+    }
     return (
-        <div>
-             <Form.Check // prettier-ignore
+        <div className="form-check">
+             {/* <Form.Check // prettier-ignore
             type="checkbox"
             id={`todo-${item.id}`}
             label={`${item.text}`}
-          />
+            className={className}
+            style={deco}
+            onChange={handleCheckboxClick}
+            checked={!isChecked}
+          /> */}
+     
+            <input className="form-check-input" type="checkbox" value="" id={`todo${item.id}`} onChange={handleCheckboxClick}/>
+            <label className={className} style={deco} htmlFor={`todo${item.id}`}>
+                {item.text}
+            </label>
+
         </div>
     );
 };
